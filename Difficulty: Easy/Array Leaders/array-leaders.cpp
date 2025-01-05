0b90f31224760314c5928083bdd4bf534a3554fd
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-01-05 15:47:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -1,6 +1,4 @@
 //{ Driver Code Starts
-// C++ program to remove recurring digits from
-// a given number
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,51 +9,57 @@ using namespace std;
 class Solution {
     // Function to find the leaders in the array.
   public:
-    vector<int> leaders(int n, int arr[]) {
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
         vector<int> ans;
-    int max_from_right = arr[n-1];  // Initialize the rightmost element as the maximum
-    ans.push_back(max_from_right);  // The rightmost element is always a leader
-
-    // Traverse the array from the second last element to the beginning
-    for (int i = n-2; i >= 0; i--) {
-        if (arr[i] >= max_from_right) {
-            max_from_right = arr[i];
-            ans.push_back(arr[i]);
+        int leader = arr[n-1];
+        ans.push_back(leader);
+        for(int i =n-2;i>=0;i--){
+                if(arr[i] >= leader){
+                    leader = arr[i];
+                    ans.push_back(leader);
+                }
         }
-    }
-
-    // The leaders are added in reverse order, reverse the result
-    reverse(ans.begin(), ans.end());
-    return ans;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
 
 int main() {
-    long long t;
-    cin >> t; // testcases
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
     while (t--) {
-        long long n;
-        cin >> n; // total size of array
+        vector<int> a;
+        string input;
 
-        int arr[n];
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
 
-        // inserting elements in the array
-        for (long long i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
         Solution obj;
-        // calling leaders() function
-        vector<int> v = obj.leaders(n, arr);
+        vector<int> result = obj.leaders(a);
 
-        // printing elements of the vector
-        for (auto it = v.begin(); it != v.end(); it++) {
-            cout << *it << " ";
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
         }
-
         cout << endl;
+        cout << "~" << endl;
     }
+
+    return 0;
 }
 
 // } Driver Code Ends
