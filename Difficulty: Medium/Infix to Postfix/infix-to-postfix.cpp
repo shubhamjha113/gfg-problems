76adfa6f83a6e2f8635c554b79bcb2b043c60b69
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-03-15 00:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// C++ implementation to convert infix expression to postfix
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int priority(char ch){
+        if(ch=='^') return 3;
+        else if(ch=='*' || ch=='/') return 2;
+        else if(ch=='+' || ch=='-') return 1;
+        else return -1;
+    }
+    string infixToPostfix(string& s) {
+        int i=0;
+        stack<int> st;
+        string ans="";
+        while(i<s.size()){
+            if((s[i]>='A'&& s[i]<='Z') || (s[i]>='a'&& s[i]<='z') || (s[i]>='0'&& s[i]<='9')){
+                ans+=s[i];
+            }
+            else if(s[i]=='(') st.push(s[i]);
+            else if(s[i]==')'){
+                while(!st.empty() && st.top()!='('){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.pop();
+            }
+            else{
+                while(!st.empty() && priority(s[i]) <= priority(st.top())){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+            i++;
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+//  Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(INT_MAX, '\n');
+    while (t--) {
+        string exp;
+        cin >> exp;
+        Solution ob;
+        cout << ob.infixToPostfix(exp) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
