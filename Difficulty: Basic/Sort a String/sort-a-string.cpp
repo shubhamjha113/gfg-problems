e0463b10ad194c9e0bd106b96616be4a4190eb1d
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-04-13 22:11:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sort a String/sort-a-string.cpp
+++ Difficulty: Basic/Sort a String/sort-a-string.cpp
@@ -24,17 +24,19 @@ return 0;
 
 string sort(string s){
     //complete the function here
-    vector<int> count(26,0);
-        for(int i=0;i<s.size() ; i++){
-            count[s[i] - 'a']++;
-        }
-        string ans;
-        for(int i=0;i<26;i++){
-            char c = 'a' + i;
-            while(count[i]){
-                ans += c;
-                count[i]--;
-            }
-        }
-        return ans;
+    // vector<int> count(26,0);
+    //     for(int i=0;i<s.size() ; i++){
+    //         count[s[i] - 'a']++;
+    //     }
+    //     string ans;
+    //     for(int i=0;i<26;i++){
+    //         char c = 'a' + i;
+    //         while(count[i]){
+    //             ans += c;
+    //             count[i]--;
+    //         }
+    //     }
+    //     return ans;
+    sort(s.begin(),s.end());
+    return s;
 }
