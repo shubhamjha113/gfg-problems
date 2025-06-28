4a538b4f4ea7e00f4044b965ce5b7efcf6e691d5
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-28 17:08:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Repeated Character/first-repeated-character.cpp
+++ Difficulty: Easy/First Repeated Character/first-repeated-character.cpp
@@ -2,16 +2,12 @@ class Solution {
   public:
     string firstRepChar(string s) {
         vector<int> count(26,0);
-        char ch;
-        bool flag = false;
         for(int i=0;i<s.size();i++){
             count[s[i]-'a']++;
             if(count[s[i]-'a']==2){
-                ch = s[i];
-                flag = true;
-                break;
+                return string(1,s[i]);
             }
         }
-        return flag?string(1,ch):"-1"; 
+        return "-1"; 
     }
 };
