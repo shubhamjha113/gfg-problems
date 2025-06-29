97ee8407435b777e5d341e0d7557d5ad37b35d93
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2025-06-29 09:34:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if Permutation is Substring/check-if-permutation-is-substring.cpp
@@ -0,0 +1,29 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool search(string& s2, string& s1) {
+        int n=s1.size();
+        int m = s2.size();
+        if(n>m) return false;
+
+        vector<int> freq1(26,0),freq2(26,0);
+        for(char &ch : s1){
+            freq1[ch-'a']++;
+        }
+
+        int i=0,j=0;
+        while(j<m){
+            freq2[s2[j]-'a']++;
+
+            if(j-i+1 > n){
+                freq2[s2[i]-'a']--;
+                i++;
+            }
+            if(freq1 == freq2) return true;
+            j++;
+        }
+
+        return false;
+        
+    }
+};
